81a63f824aa95bef932a4c684cdb875ae33a8825
Daniel <Razdaniel25@gmail.com>
2024-07-23 15:43:40 +0300

    Integration Voyage Tour

--- layouts/users/tour/TimelineEvent.js
+++ layouts/users/tour/TimelineEvent.js
@@ -1,14 +1,7 @@
 import { Timeline } from 'primereact/timeline';
 import style from './../../../style/layouts/users/tour/TimelineEvent.module.css';
-export default function TimelineEvent() {
 
-    const events = [
-        {date:"14-07-2024",name:"Antananarivo - Miandrivazo with 4X4",description:"ad labore impedit incidunt tempora sed aliquid qui totam ducimus distinctio, quos minus aliquam tempore officiis dolorem quidem amet mollitia, est fugiat. Numquam nihil dolor corrupti, repellat temporibus facilis quisquam odio quam nobis doloribus harum earum quod nostrum voluptatum quia enim iure? Quae molestias magni sint aliquid rerum, veniam aperiam? Voluptas molestias fugiat doloremque dolorum?"},
-        {date:"15-07-2024",name:"Antananarivo - Miandrivazo with 4X4",description:"ad labore impedit incidunt tempora sed aliquid qui totam ducimus distinctio, quos minus aliquam tempore officiis dolorem quidem amet mollitia, est fugiat. Numquam nihil dolor corrupti, repellat temporibus facilis quisquam odio quam nobis doloribus harum earum quod nostrum voluptatum quia enim iure? Quae molestias magni sint aliquid rerum, veniam aperiam? Voluptas molestias fugiat doloremque dolorum?"},
-        {date:"17-07-2024",name:"Antananarivo - Miandrivazo with 4X4",description:"ad labore impedit incidunt tempora sed aliquid qui totam ducimus distinctio, quos minus aliquam tempore officiis dolorem quidem amet mollitia, est fugiat. Numquam nihil dolor corrupti, repellat temporibus facilis quisquam odio quam nobis doloribus harum earum quod nostrum voluptatum quia enim iure? Quae molestias magni sint aliquid rerum, veniam aperiam? Voluptas molestias fugiat doloremque dolorum?"},
-        {date:"18-07-2024",name:"Antananarivo - Miandrivazo with 4X4",description:"ad labore impedit incidunt tempora sed aliquid qui totam ducimus distinctio, quos minus aliquam tempore officiis dolorem quidem amet mollitia, est fugiat. Numquam nihil dolor corrupti, repellat temporibus facilis quisquam odio quam nobis doloribus harum earum quod nostrum voluptatum quia enim iure? Quae molestias magni sint aliquid rerum, veniam aperiam? Voluptas molestias fugiat doloremque dolorum?"}
-    ]
-
+export default function TimelineEvent({trajets}) {
     const numero = (item,index) =>{
         return <span className={style.numero}>{index+1}</span>
     }
@@ -17,11 +10,11 @@ export default function TimelineEvent() {
         return(
             <div className={style.evenement_container}>
                 <div className={style.evenement_head}>
-                    <span className={style.evenementDate}>{item.date}</span>
-                    <span className={style.evenementTitle}>{item.name}</span>
+                    <span className={style.evenementDate}>{item.date_trajet}</span>
+                    <span className={style.evenementTitle}>{item.nom_ville}</span>
                 </div>
-                <div className={style.description}>
-                    {item.description}
+                 <div className={style.description}>
+                    {item.description_trajet || 'No description available'}
                 </div>
             </div>
         )
@@ -32,7 +25,7 @@ export default function TimelineEvent() {
             connector:{className:style.connector}
         }}
         className={style.container}
-        value={events} 
+        value={trajets} 
         align='left'
         marker={numero}
         content={evenementTemplate}
