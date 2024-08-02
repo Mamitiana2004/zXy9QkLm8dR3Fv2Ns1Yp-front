4eef43eb0d57b5880ab715d2f31d24a7d26d865b
Brad Akemi <mirado.akemi@gmail.com>
2024-08-02 18:19:38 +0300

    add image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      images: {
-    domains: ['192.168.88.57','192.168.88.23','34.122.163.41','192.168.88.13'],
+  images: {
+    domains: ['192.168.88.57', '192.168.88.23', '34.122.163.41', '192.168.88.13', 'admin-server.craft-aftrip.com'],
   },
 };
 
