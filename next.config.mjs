105b6ceddeb1d4d21b680ebe1dfa80aad39310c7
AkemiFF <mirado.akemi@gmail.com>
2024-08-02 17:36:33 +0000

    conflit resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
     domains: ['127.0.0.1', '192.168.88.57', '192.168.88.23', '34.122.163.41', '192.168.88.13', 'admin-server.craft-aftrip.com'],
-=======
-      images: {
-    domains: ['192.168.88.57','192.168.88.23','34.122.163.41','192.168.88.13', 'admin-server.craft-aftrip.com'],
->>>>>>> 6aecacc (serveur)
   },
 };
 
