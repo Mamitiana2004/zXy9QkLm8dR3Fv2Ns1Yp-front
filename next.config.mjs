e18b304d0575dfd98c1c79befe354f25a7c8c5a8
AkemiFF <mirado.akemi@gmail.com>
2024-08-02 17:24:11 +0000

    serveur

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   images: {
     domains: ['127.0.0.1', '192.168.88.57', '192.168.88.23', '34.122.163.41', '192.168.88.13', 'admin-server.craft-aftrip.com'],
+=======
+      images: {
+    domains: ['192.168.88.57','192.168.88.23','34.122.163.41','192.168.88.13', 'admin-server.craft-aftrip.com'],
+>>>>>>> 6aecacc (serveur)
   },
 };
 
