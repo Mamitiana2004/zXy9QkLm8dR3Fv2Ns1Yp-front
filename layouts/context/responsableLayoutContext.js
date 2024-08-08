4f9bd37bd0ce4e558c6e5969158587fb149115bc
Brad Akemi <mirado.akemi@gmail.com>
2024-08-08 22:26:58 +0300

    correction no responsable

--- layouts/context/responsableLayoutContext.js
+++ layouts/context/responsableLayoutContext.js
@@ -7,7 +7,8 @@ export const ResponsableLayoutProvider = ({ children }) => {
     const [user, setUser] = useState();
     const [typeResponsable, setTypeResponsable] = useState(1);
 
-    return <ResponsableLayoutContext.Provider value={{ user, setUser, typeResponsable, setTypeResponsable }}>{children}</ResponsableLayoutContext.Provider>
+    return <ResponsableLayoutContext.Provider
+        value={{ user, setUser, typeResponsable, setTypeResponsable }}>{children}</ResponsableLayoutContext.Provider>
 }
 
 export default ResponsableLayoutContext;
