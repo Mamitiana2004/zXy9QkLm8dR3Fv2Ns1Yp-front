26545cb9584205742ff1c870b56220eba5eae18e
Diablo <Razdaniel25@gmail.com>
2024-08-09 21:00:18 +0300

    Integration final resposable/accommodation/booking

--- pages/responsable/accommodation/booking.js
+++ pages/responsable/accommodation/booking.js
@@ -1,34 +1,22 @@
 import Head from "next/head";
 import style from '@/style/pages/responsable/accommodation/booking.module.css';
-import { Button } from "primereact/button";
+import { useState, useEffect, useContext } from "react";
 import RoomPlanning from "@/components/Planning";
-import { useEffect, useState, useContext } from "react";
 import UrlConfig from "@/util/config";
-import { useRouter } from 'next/router';
 import ResponsableLayoutContext from "@/layouts/context/responsableLayoutContext";
-import { custom_login, getCsrfTokenDirect } from '@/util/csrf';
+import { getCsrfTokenDirect } from '@/util/csrf';
 
 export default function Booking() {
     const [book, setBook] = useState([]);
+    const [rooms, setRooms] = useState([]);
     const { user } = useContext(ResponsableLayoutContext);
-    const id = user ? user.id_hebergement : 0;
-
-    const [rooms, setRooms] = useState([
-        { id: 1, title: "Room 401" },
-        { id: 2, title: "Room 402" },
-        { id: 3, title: "Room 403" },
-        { id: 4, title: "Room 404" },
-        { id: 5, title: "Room 405" },
-        { id: 6, title: "Room 406" },
-        { id: 7, title: "Room 407" },
-        { id: 8, title: "Room 408" }
-    ]);
-
+    const id = user ? user.id_hebergement : 1;
 
     useEffect(() => {
         if (!id) return; // Ensure the ID is available before making the request
 
         getCsrfTokenDirect().then((csrfToken) => {
+            // Fetch booking data
             fetch(`${UrlConfig.apiBaseUrl}/api/hebergement/reservations/${id}/`, {
                 method: "GET",
                 headers: {
@@ -47,6 +35,24 @@ export default function Booking() {
                         resourceId: item.chambre_reserve
                     }));
                     setBook(mappedData);
+                })
+                .catch(err => console.log(err));
+
+            // Fetch room data
+            fetch(`${UrlConfig.apiBaseUrl}/api/hebergement/${id}/chambres/`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFTOKEN': csrfToken,
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    const mappedRooms = data.map(room => ({
+                        id: room.id,
+                        title: room.nom_chambre
+                    }));
+                    setRooms(mappedRooms);
                 })
                 .catch(err => console.log(err));
         });
