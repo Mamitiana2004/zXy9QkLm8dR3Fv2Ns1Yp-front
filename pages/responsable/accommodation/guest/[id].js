9087bd5fabb7a00d74ef8b3a0c475c169fc04df1
Diablo <Razdaniel25@gmail.com>
2024-08-11 15:55:07 +0300

    Integration final partie Guest et Guest Details / Accommodation
    Addimage

--- pages/responsable/accommodation/guest/[id].js
+++ pages/responsable/accommodation/guest/[id].js
@@ -4,7 +4,7 @@ import style from './../../../../style/pages/responsable/accommodation/guest/id.
 import Link from "next/link";
 import { Image } from "primereact/image";
 import { Button } from "primereact/button";
-import { useEffect, useState , useContext} from "react";
+import { useEffect, useState, useContext } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import ResponsableLayoutContext from "@/layouts/context/responsableLayoutContext";
@@ -13,38 +13,25 @@ import UrlConfig from "@/util/config";
 
 export default function DetailGuest() {
     const router = useRouter();
-    const [guests, setGuests] = useState()
+    const [guests, setGuests] = useState(null);
     const [nameHotel, setNameHotel] = useState(null); 
-    const [bookingSummary, setBookingSamurry] = useState([]);
+    const [bookingSummary, setBookingSummary] = useState({});
+    const [bookingHistory, setBookingHistory] = useState([]);
+    const [images, setImages] = useState([]);
     const { user } = useContext(ResponsableLayoutContext);
     const { id } = router.query;
 
-
-    const [bookingHistory, setBookingHistory] = useState([
-        { id: 1, name: "Will Smith", email: "willsmith@gmail.com", nbr_guest: 1, room: "404", check_in: "07-07-2024", check_out: "08-07-2024" },
-        {id:1,name:"Will Smith",email:"willsmith@gmail.com",nbr_guest:1,room:"404",check_in:"07-07-2024",check_out:"08-07-2024"},
-        { id: 1, name: "Will Smith", email: "willsmith@gmail.com", nbr_guest: 1, room: "404", check_in: "07-07-2024", check_out: "08-07-2024" }
-    ]);
-
     useEffect(() => {
         if (user && id) {
-           
             const id_hebergement = user.id_etablissement;
-
-            // Mode debug
-            console.log('User:', user);
-            console.log('id_hebergement:', id_hebergement);
-            console.log('client:', id);
             FetchDetails_Guest(id_hebergement, id);
-
-           
-
         }
-    }, [user,id]);
+    }, [user, id]);
 
-    function FetchDetails_Guest(id_hebergement) {
+    function FetchDetails_Guest(id_hebergement, id) {
         getCsrfFromToken()
             .then(csrfToken => {
+                // Fetch hotel details
                 fetch(`${UrlConfig.apiBaseUrl}/api/hebergement/get-id-hebergement/${id_hebergement}/`, {
                     method: "GET",
                     headers: {
@@ -58,7 +45,7 @@ export default function DetailGuest() {
                     })
                     .catch(err => console.error('Erreur lors de la récupération du nom de l\'hôtel:', err));
                     
-                // Fetch DetailGuest
+                // Fetch guest details
                 fetch(`${UrlConfig.apiBaseUrl}/api/hebergement/client-reservations/${id}/hebergement/${id_hebergement}/`, {
                     method: "GET",
                     headers: {
@@ -67,28 +54,32 @@ export default function DetailGuest() {
                     }
                 })
                     .then(response => response.json())
-                    .then(GuestData => {
-                        setGuests(GuestData);
-                        setBookingSamurry(GuestData);
+                    .then(guestData => {
+                        setGuests(guestData);
+                        setBookingSummary(guestData.reservations[0]);
                         setBookingHistory(
-                            GuestData.reservations.map(res => ({
+                            guestData.reservations.map(res => ({
                                 id: res.client.id,
                                 name: res.client.nom,
                                 email: res.client.email,
                                 nbr_guest: res.nombre_personnes_reserve,
                                 room: res.chambre.nom,
-                                check_in: new Date(res.date_debut_reserve).toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' }),
-                                check_out: new Date(res.date_fin_reserve).toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' })
+                                check_in: res.date_debut_reserve,
+                                check_out: res.date_fin_reserve
                             }))
-                        )
+                        );
                     })
                     .catch(err => console.error('Erreur lors de la récupération des Clients de l\'hôtel:', err));
-
             })
             .catch(err => console.error('Erreur lors de la récupération du token CSRF:', err));   
     }
 
-    return(
+      // Get the first image URL from the guests data
+    const firstImageUrl = guests?.reservations?.[0]?.chambre?.images?.[0]?.url;
+    console.log(firstImageUrl); // Log the images
+
+
+    return (
         <>
             <Head>
                 <title>Detail Guest</title>
@@ -103,16 +94,29 @@ export default function DetailGuest() {
 
             <div className={style.container}>
                 <Link className={style.container_title_link} href={"/responsable/accommodation/guest"}>
-                    <i className="pi pi-arrow-left"/>
+                    <i className="pi pi-arrow-left" />
                     <span>Back</span>
                 </Link>
                 <div className={style.card_container}>
                     <div className={style.guest_information_container}>
                         <div className={style.guest_information_left_container}>
                             <div className={style.image_guest_container}>
-                                <Image src="/images/hotel/chambre.jpg" alt="chambre" imageClassName={style.image_guest_room}/>
-                                <Button onClick={()=>router.push("/responsable/accommodation/room")} raised label="See all room" className="button-secondary"/>
-                                <Button onClick={()=>router.push("/responsable/accommodation/guest")} raised label="See all guest" className="button-secondary"/>
+                                 {firstImageUrl ? (
+                                    <Image 
+                                        src={UrlConfig.apiBaseUrl+firstImageUrl} 
+                                        alt="Chambre" 
+                                        imageClassName={style.image_guest_room}
+                                        onError={(e) => e.currentTarget.src = '/images/hotel/chambre.jpg'}
+                                    />
+                                ) : (
+                                    <Image 
+                                        src="/images/hotel/chambre.jpg" 
+                                        alt="Chambre" 
+                                        imageClassName={style.image_guest_room}
+                                    />
+                                )}
+                                <Button onClick={() => router.push("/responsable/accommodation/room")} raised label="See all rooms" className="button-secondary" />
+                                <Button onClick={() => router.push("/responsable/accommodation/guest")} raised label="See all guests" className="button-secondary" />
                             </div>
                             <div className={style.guest_information}>
                                 <div className={style.user_container}>
@@ -144,10 +148,6 @@ export default function DetailGuest() {
                                             <span className={style.guest_detail_title}>Guest request</span>
                                             <span className={style.guest_detail_value}>{guests?.reservations?.[0]?.nombre_personnes_reserve || 'No Guest number'}</span>
                                         </div>
-                                        {/* <div className={style.guest_detail}>
-                                            <span className={style.guest_detail_title}>Room Type</span>
-                                            <span className={style.guest_detail_value}>401</span>
-                                        </div> */}
                                     </div>
                                 </div>
                             </div>
@@ -156,28 +156,28 @@ export default function DetailGuest() {
                             <span className={style.guest_information_right_title}>Booking summary</span>
                             <div className={style.guest_information_right}>
                                 <span className={style.guest_information_right_label}>
-                                    <i className="pi pi-clock"/>
-                                    Periode
+                                    <i className="pi pi-clock" />
+                                    Period
                                 </span>
-                                <span className={style.guest_information_right_value}>{bookingSummary?.reservations?.[0].nombre_de_nuits || 'No number nights'} nights</span>
+                                <span className={style.guest_information_right_value}>{bookingSummary?.nombre_de_nuits || 'No number nights'} nights</span>
                             </div>
                             <div className={style.guest_information_right}>
                                 <span className={style.guest_information_right_label}>
-                                    <i className="pi pi-user"/>
+                                    <i className="pi pi-user" />
                                     Guest
                                 </span>
-                                <span className={style.guest_information_right_value}>{bookingSummary?.reservations?.[0]?.nombre_personnes_reserve || 'No Guest number'}</span>
+                                <span className={style.guest_information_right_value}>{bookingSummary?.nombre_personnes_reserve || 'No Guest number'}</span>
                             </div>
                             <div className={style.guest_information_right}>
                                 <span className={style.guest_information_right_label}>
-                                    <i className="pi pi-wallet"/>
+                                    <i className="pi pi-wallet" />
                                     Price
                                 </span>
-                                <span className={style.guest_information_right_value}>$ {bookingSummary?.reservations?.[0]?.chambre.prix_par_nuit || 'No price'} / nights</span>
+                                <span className={style.guest_information_right_value}>$ {bookingSummary?.chambre?.prix_par_nuit || 'No price'} / night</span>
                             </div>
                             <div className={style.guest_information_right}>
                                 <span className={style.guest_information_right_label}>
-                                    <i className="pi pi-ticket"/>
+                                    <i className="pi pi-ticket" />
                                     Discount
                                 </span>
                                 <span className={style.guest_information_right_value}>0</span>
@@ -188,23 +188,23 @@ export default function DetailGuest() {
                                 <span className={style.guest_information_right_label}>
                                     Total
                                 </span>
-                                <span className={style.guest_information_right_value_active}>$ {bookingSummary?.reservations?.[0]?.prix_total_reserve || 'No price'}</span>
+                                <span className={style.guest_information_right_value_active}>$ {bookingSummary?.prix_total_reserve || 'No price'}</span>
                             </div>
                         </div>
                     </div>
                     <div className={style.separateur}></div>
                     <h2>Booking history</h2>
-                    <DataTable  value={bookingHistory}>
+                    <DataTable value={bookingHistory}>
                         <Column sortable field="id" header="No"/>
                         <Column sortable field="name" header="Name"/>
                         <Column sortable field="email" header="Email"/>
-                        <Column sortable field="nbr_guest" header="No. Guest"/>
+                        <Column sortable field="nbr_guest" header="Nbr guest"/>
                         <Column sortable field="room" header="Room"/>
-                        <Column sortable field="check_in" header="Check in"/>
-                        <Column sortable field="check_out" header="Check out"/>
+                        <Column sortable field="check_in" header="Check-in"/>
+                        <Column sortable field="check_out" header="Check-out"/>
                     </DataTable>
                 </div>
             </div>
         </>
-    )
+    );
 }
