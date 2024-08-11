cd9b5b110648aeefd3ab73135e1d291dbbcf159b
Diablo <Razdaniel25@gmail.com>
2024-08-11 13:48:22 +0300

    Integration Name Hotel et Total  / Dashboard

--- pages/responsable/accommodation/dashboard.js
+++ pages/responsable/accommodation/dashboard.js
@@ -2,11 +2,17 @@ import Head from "next/head";
 import style from './../../../style/pages/responsable/accommodation/dahsboard.module.css'
 import { Button } from "primereact/button";
 import { Chart } from "primereact/chart";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Calendar } from "primereact/calendar";
 import { useRouter } from "next/router";
+import { getCsrfFromToken } from '@/util/csrf';
+import ResponsableLayoutContext from "@/layouts/context/responsableLayoutContext";
+import UrlConfig from "@/util/config";
+
+
+
 export default function DashBoard() {
 
     const router = useRouter();
@@ -26,6 +32,63 @@ export default function DashBoard() {
         {id:"#41",name:"Paul Adamas",room:"203",guests:"2",check_in:"07-07-2024",check_out:"08-07-2024"},
         {id:"#41",name:"Paul Adamas",room:"203",guests:"2",check_in:"07-07-2024",check_out:"08-07-2024"}
     ])
+
+     // Integration 
+    const { user } = useContext(ResponsableLayoutContext);
+
+    const [nameHotel, setNameHotel] = useState(null);
+    const [totalBookings, setTotalBookings] = useState(0);
+    const [totalGuest, setTotalGuest] = useState(0);
+    const [totalRooms, setTotalRooms] = useState(0);
+    
+    useEffect(() => { 
+        if(user){
+            const id_hebergement = user.id_etablissement;
+
+            // Mode debug
+            // console.log('User:', user);
+            // console.log('id_hebergement:', id_hebergement);
+
+            FetchDashboard_Hotel(id_hebergement);
+        }
+    }, [user])
+
+    function FetchDashboard_Hotel(id_hebergement) { 
+    getCsrfFromToken()
+        .then(csrfToken => {
+            // Fetch Hotel Data
+            fetch(`${UrlConfig.apiBaseUrl}/api/hebergement/get-id-hebergement/${id_hebergement}/`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFTOKEN': csrfToken,
+                }
+            })
+            .then(response => response.json())
+            .then(hotelData => {
+                setNameHotel(hotelData);
+            })
+            .catch(err => console.error('Erreur lors de la récupération du nom de l\'hôtel:', err));
+            
+            // Fetch Hotel Statistics
+            fetch(`${UrlConfig.apiBaseUrl}/api/hebergement/${id_hebergement}/stats/`, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFTOKEN': csrfToken,
+                }
+            })
+            .then(response => response.json())
+            .then(Total => {
+                setTotalBookings(Total);
+                setTotalGuest(Total);
+                setTotalRooms(Total);
+            })
+            .catch(err => console.error('Erreur lors de la récupération des statistiques de l\'hôtel:', err));
+        })
+        .catch(err => console.error('Erreur lors de la récupération du token CSRF:', err));   
+}
+
 
     useEffect(()=>{
         const data = {
@@ -192,7 +255,7 @@ export default function DashBoard() {
             <div className={style.top_container}>
                 <div className={style.top_container_title_container}>
                     <span className={style.top_container_title}>Dashboard</span>
-                    <span className={style.top_container_subtitle}>Brajas Hotel</span>
+                    <span className={style.top_container_subtitle}>{nameHotel?.nom_hebergement || 'No Hotel Name'}</span>
                 </div>
                 <Button onClick={()=>router.push("/responsable/accommodation/addNewRoom")} label="+ Add new room" className={style.button_add}/>
             </div>
@@ -205,21 +268,21 @@ export default function DashBoard() {
                             <i className="pi pi-calendar" style={{fontSize:"32px"}}/>
                             <div className={style.card_detail_text}>
                                 <span className={style.card_detail_title}>Total booking</span>
-                                <span className={style.card_detail_value}>32</span>
+                                <span className={style.card_detail_value}>{totalBookings.booking_count}</span>
                             </div>
                         </div>
                         <div className={style.card_detail}>
                             <i className="pi pi-calendar" style={{fontSize:"32px"}}/>
                             <div className={style.card_detail_text}>
                                 <span className={style.card_detail_title}>Avaliable room</span>
-                                <span className={style.card_detail_value}>3</span>
+                                <span className={style.card_detail_value}>{totalRooms.available_room_count}</span>
                             </div>
                         </div>
                         <div className={style.card_detail}>
                             <i className="pi pi-calendar" style={{fontSize:"32px"}}/>
                             <div className={style.card_detail_text}>
                                 <span className={style.card_detail_title}>Total guests</span>
-                                <span className={style.card_detail_value}>75</span>
+                                <span className={style.card_detail_value}>{totalGuest.total_guests}</span>
                             </div>
                         </div>
                     </div>
