e9f9d614471f0b8c3241e606796fa06b5d24b603
Brad Akemi <mirado.akemi@gmail.com>
2024-09-09 17:43:42 +0300

    add log out responsable

--- layouts/context/responsableLayoutContext.js
+++ layouts/context/responsableLayoutContext.js
@@ -3,23 +3,14 @@ import { createContext, useEffect, useState } from "react";
 
 const ResponsableLayoutContext = createContext();
 
-// export const ResponsableLayoutProvider = ({ children }) => {
-
-//     const [user, setUser] = useState();
-//     const [typeResponsable, setTypeResponsable] = useState(1);
-
-//     return <ResponsableLayoutContext.Provider
-//         value={{ user, setUser, typeResponsable, setTypeResponsable }}>{children}</ResponsableLayoutContext.Provider>
-// }
-
-
 export const ResponsableLayoutProvider = ({ children }) => {
-    const [user, setUser] = useState();
+    const [user, setUser] = useState(null);
     const [typeResponsable, setTypeResponsable] = useState(1);
 
     useEffect(() => {
         const savedUser = localStorage.getItem('responsable_user');
         const savedTypeResponsable = localStorage.getItem('type_responsable');
+
         if (savedUser) {
             setUser(JSON.parse(savedUser));
         }
@@ -27,10 +18,34 @@ export const ResponsableLayoutProvider = ({ children }) => {
             setTypeResponsable(parseInt(savedTypeResponsable, 10));
         }
     }, []);
+    useEffect(() => {
+        const checkAccessToken = async () => {
+            try {
+                const token = await getResponsableAccessToken();
+                if (!token) {
+                    handleLogout();
+                }
+            } catch (error) {
+                handleLogout();
+            }
+        };
+
+        checkAccessToken();
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('responsable_user');
+        localStorage.removeItem('type_responsable');
+        setUser(null);
+        removeAccessResponsable();
+    };
 
     useEffect(() => {
         if (user) {
             localStorage.setItem('responsable_user', JSON.stringify(user));
+        } else {
+            localStorage.removeItem('responsable_user');
+            localStorage.removeItem('type_responsable');
         }
     }, [user]);
 
@@ -38,43 +53,6 @@ export const ResponsableLayoutProvider = ({ children }) => {
         localStorage.setItem('type_responsable', typeResponsable);
     }, [typeResponsable]);
 
-    useEffect(() => {
-        const checkAccessToken = async () => {
-            try {
-                const token = await getResponsableAccessToken();
-                if (!token) {
-                    localStorage.removeItem('type_responsable');
-                    localStorage.removeItem('responsable_user');
-                    setUser(null);
-                }
-            } catch (error) {
-                localStorage.removeItem('responsable_user');
-                localStorage.removeItem('type_responsable');
-                setUser(null);
-            }
-        };
-
-        checkAccessToken();
-    }, []);
-
-    useEffect(() => {
-        const clearUser = async () => {
-            try {
-                if (!user) {
-                    localStorage.removeItem('type_responsable');
-                    localStorage.removeItem('responsable_user');
-                    setUser(null);
-                    removeAccessResponsable();
-                }
-            } catch (error) {
-                localStorage.removeItem('responsable_user');
-                localStorage.removeItem('type_responsable');
-                setUser(null);
-                removeAccessResponsable();
-            }
-        };
-        clearUser();
-    }, [user]);
     return (
         <ResponsableLayoutContext.Provider
             value={{ user, setUser, typeResponsable, setTypeResponsable }}
@@ -84,7 +62,4 @@ export const ResponsableLayoutProvider = ({ children }) => {
     );
 };
 
-
 export default ResponsableLayoutContext;
-
-
