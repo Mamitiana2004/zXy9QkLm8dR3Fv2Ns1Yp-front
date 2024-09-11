573f6c8dbdc472f07760d1d5aa52ca7325913e9e
Brad Akemi <mirado.akemi@gmail.com>
2024-09-11 11:43:13 +0300

    correction setting accomodations

--- pages/responsable/accommodation/setting/log.js
+++ pages/responsable/accommodation/setting/log.js
@@ -1,23 +1,25 @@
 import Head from "next/head";
 import style from './../../../../style/pages/responsable/accommodation/setting.module.css';
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Button } from "primereact/button";
 import { useRouter } from "next/router";
+import ResponsableLayoutContext from "@/layouts/context/responsableLayoutContext";
 export default function Log() {
 
-    const router= useRouter();
+    const router = useRouter();
+    const { user, setUser } = useContext(ResponsableLayoutContext);
 
-    const [menuSidebar,setMenuSidebar] = useState([
-        {label:"Profil"},
-        {label:"Security"},
-        {label:"Notification"},
-        {label:"Log"},
-        {label:"Help"}
+    const [menuSidebar, setMenuSidebar] = useState([
+        { label: "Profil" },
+        { label: "Commission" },
+        { label: "Notification" },
+        { label: "Log" },
+        { label: "Help" }
     ]);
 
     const menu = 3;
 
-    return(
+    return (
         <>
             <Head>
                 <title>Log</title>
@@ -27,11 +29,11 @@ export default function Log() {
                 <div className={style.left_container}>
                     <div className={style.left_top_container}>
                         <span className={style.left_top_subtitle}>{menuSidebar[menu].label}</span>
-                        <span className={style.left_top_title}>Tik&apos;Art</span>
+                        <span className={style.left_top_title}>{user ? user.nom_hebergement : null}</span>
                     </div>
                     <div className={style.left_body_container}>
-                        {menuSidebar.map((item,index)=>{
-                            return <Button key={index} onClick={()=>router.push("/responsable/accommodation/setting/"+item.label.toLowerCase())} text className={menu==index ? "button-secondary" : style.text_button} raised={menu==index ? true : false} label={item.label}/>
+                        {menuSidebar.map((item, index) => {
+                            return <Button key={index} onClick={() => router.push("/responsable/accommodation/setting/" + item.label.toLowerCase())} text className={menu == index ? "button-secondary" : style.text_button} raised={menu == index ? true : false} label={item.label} />
                         })}
                     </div>
                 </div>
